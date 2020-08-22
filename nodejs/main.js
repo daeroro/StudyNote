6ffec3fb54978c715b8b12859277c20591210d74
Daero Lee <skseofh@gmail.com>
2020-08-23 00:01:46 +0900

    200822

--- nodejs/main.js
+++ nodejs/main.js
@@ -3,40 +3,8 @@ var fs = require('fs');
 var url = require('url');
 // http, fs, url -> node js의 모듈
 var qs = require('querystring');
+var template = require('./lib/template.js');
 
-var template = {
-  list:function(filelist)
-  {
-    var list = `<ul>`;
-    var i = 0;
-    while(i<filelist.length)
-    {
-      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-      i = i+1;
-    }
-  
-    list = list + '<ul>';
-  
-    return list;
-  },
-  HTML:function(title, list, body, control){
-    return `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      ${list}
-      ${control}
-      <p>${body}</p>
-    </body>
-    </html>
-    `;
-  }
-}
 
 var app = http.createServer(function(request,response){
   var _url = request.url;
